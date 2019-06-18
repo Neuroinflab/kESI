e4a90e872508dddb49473e24eab7339494895132
Jakub Dzik <abukaj@users.noreply.github.com>
2019-06-18 12:35:02 +0200

    `colorblind_friendly` module refactored
    Color RGB definitions represented internally as `_Color` `namedtuple`.

--- colorblind_friendly.py
+++ colorblind_friendly.py
@@ -12,14 +12,29 @@
 # 11.20.2002 (modified on 2.15.2008, 9.24.2008)
 # http://jfly.iam.u-tokyo.ac.jp/color/#pallet
 
+import collections
+
+
+_Color = collections.namedtuple('_Color', ['red', 'green', 'blue'])
+
 def _html(r, g, b):
     return "#{:02X}{:02X}{:02X}".format(r, g, b)
 
-BLACK     = _html(  0,   0,   0)
-ORANGE    = _html(230, 159,   0)
-SKY_BLUE  = _html( 86, 180, 233)
-GREEN     = _html(  0, 158, 115)
-YELLOW    = _html(240, 228,  66)
-BLUE      = _html(  0, 114, 178)
-VERMILION = _html(213,  94,   0)
-PURPLE    = _html(204, 121, 167)
+
+_BLACK     = _Color(  0,   0,   0)
+_ORANGE    = _Color(230, 159,   0)
+_SKY_BLUE  = _Color( 86, 180, 233)
+_GREEN     = _Color(  0, 158, 115)
+_YELLOW    = _Color(240, 228,  66)
+_BLUE      = _Color(  0, 114, 178)
+_VERMILION = _Color(213,  94,   0)
+_PURPLE    = _Color(204, 121, 167)
+
+BLACK     = _html(*_BLACK)
+ORANGE    = _html(*_ORANGE)
+SKY_BLUE  = _html(*_SKY_BLUE)
+GREEN     = _html(*_GREEN)
+YELLOW    = _html(*_YELLOW)
+BLUE      = _html(*_BLUE)
+VERMILION = _html(*_VERMILION)
+PURPLE    = _html(*_PURPLE)
