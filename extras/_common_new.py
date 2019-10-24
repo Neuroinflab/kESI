2ced4cb4051fc70da5ab7f73b1f2420b8949328d
Jakub Dzik <abukaj@users.noreply.github.com>
2019-10-24 14:56:15 +0200

    New API of `GaussianSourceKCSD3D` class.
    A new (temporary) module `_common_new` provided, intended to replace
    module `common` in the 'extras' directory.  At the moment the module
    contains `GaussianSourceBase` and `GaussianSourceKCSD3D` classes.
    The classes has been reimplemented, as management of measurement nodes
    since 781b58602b1c2a12dcf150d97225bf560b90c8ae is not their
    responsibility.

---
+++ extras/_common_new.py
@@ -0,0 +1,46 @@
+import logging
+
+import numpy as np
+from scipy.special import erf
+
+
+logger = logging.getLogger(__name__)
+
+
+class GaussianSourceBase(object):
+    def __init__(self, x, y, z, standard_deviation):
+        self.x = x
+        self.y = y
+        self.z = z
+        self._variance = standard_deviation ** 2
+        self._a = (2 * np.pi * self._variance) ** -1.5
+
+
+class GaussianSourceKCSD3D(GaussianSourceBase):
+    _dtype = np.sqrt(0.5).__class__
+    _fraction_of_erf_to_x_limit_in_0 = _dtype(2 / np.sqrt(np.pi))
+    _x = _dtype(1.)
+    _half = _dtype(0.5)
+    _last = 2.
+    _err = 1.
+    while 0 < _err < _last:
+        _radius_of_erf_to_x_limit_applicability = _x
+        _last = _err
+        _x *= _half
+        _err = _fraction_of_erf_to_x_limit_in_0 - erf(_x) / _x
+
+    def __init__(self, x, y, z, standard_deviation, conductivity):
+        super(GaussianSourceKCSD3D, self).__init__(x, y, z, standard_deviation)
+        self.conductivity = conductivity
+        self._b = 0.25 / (np.pi * conductivity)
+        self._c = np.sqrt(0.5) / standard_deviation
+
+    def csd(self, X, Y, Z):
+        return self._a * np.exp(-0.5 * ((X - self.x) ** 2 + (Y - self.y) ** 2 + (Z - self.z) ** 2)/self._variance)
+
+    def potential(self, X, Y, Z):
+        R = np.sqrt((X - self.x) ** 2 + (Y - self.y) ** 2 + (Z - self.z) ** 2)
+        Rc = R * self._c
+        return self._b * np.where(Rc >= self._radius_of_erf_to_x_limit_applicability,
+                                  erf(Rc) / R,
+                                  self._c * self._fraction_of_erf_to_x_limit_in_0)
