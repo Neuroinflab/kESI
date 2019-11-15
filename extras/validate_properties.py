3c1b873f6622535682ee982b4085d32bcf43e819
m-kowalska <m.kowalska@nencki.gov.pl>
2019-11-15 17:13:30 +0100

    basic visualization of eigensources for 2d slice

--- extras/validate_properties.py
+++ extras/validate_properties.py
@@ -4,6 +4,9 @@
 
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib.gridspec as gridspec
+import matplotlib.cm as cm
 
 import kesi
 from kesi._verbose import VerboseFFR
@@ -11,9 +14,9 @@ from _common_new import GaussianSourceKCSD3D
 
 
 class ValidateKESI(VerboseFFR):
-    def _eigensources(self, measurement_manager, regularization_parameter=0):
+    def _eigensources(self, measurement_manager_basis, regularization_parameter=0):
         kernel = self.kernel
-        cross_kernel = self.cross_kernel(measurement_manager)
+        cross_kernel = self.cross_kernel(measurement_manager_basis)
         self.eigenvalues, self.eigenvectors = self._evd(kernel,
                                                         regularization_parameter)
         return np.dot(cross_kernel, self.eigenvectors)
@@ -21,6 +24,9 @@ class ValidateKESI(VerboseFFR):
     def _evd(self, K, regularization_parameter=0):
         eigenvalues, eigenvectors = np.linalg.eigh(K + np.identity(K.shape[0])
                                                    * regularization_parameter)
+        idx = eigenvalues.argsort()[::-1]
+        eigenvalues = eigenvalues[idx]
+        eigenvectors = eigenvectors[:, idx]
         return eigenvalues, eigenvectors
 
 
@@ -60,4 +66,29 @@ sources = gaussian_source_factory_2d(src_X.flatten(),
 
 measurement_manager = MeasurementManager(ELECTRODES)
 reconstructor = ValidateKESI(sources, measurement_manager)
-eigensources = reconstructor._eigensources(measurement_manager)
+
+est_X, est_Y = np.mgrid[0.:1.:100j,
+                        0.:1.:100j]
+EST_POINTS = pd.DataFrame({'X': est_X.flatten(),
+                           'Y': est_Y.flatten(),
+                           })
+measurement_manager_basis = MeasurementManager(EST_POINTS)
+eigensources = reconstructor._eigensources(measurement_manager_basis)
+
+fig = plt.figure(figsize=(18, 16))
+heights = [1, 1, 1, 1]
+
+gs = gridspec.GridSpec(4, 4, height_ratios=heights, hspace=0.6, wspace=0.5)
+nr_plts = 16
+
+for i in range(nr_plts):
+    ax = fig.add_subplot(gs[i], aspect='equal')
+
+    a = eigensources[:, i].reshape(len(est_X), len(est_X), 1)
+    cset = ax.contourf(est_X, est_Y, a[:, :, 0], cmap=cm.bwr)
+
+    ax.text(0.5, 1.05, r"$\tilde{K} \cdot{v_{{%(i)d}}}$" % {'i': i+1},
+            horizontalalignment='center', transform=ax.transAxes,
+            fontsize=15)
+
+            
