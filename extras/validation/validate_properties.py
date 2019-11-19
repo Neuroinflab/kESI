af3aa93e80629f46faff1d6b7eeeb6cbd11cd08b
m-kowalska <m.kowalska@nencki.gov.pl>
2019-11-19 14:48:19 +0100

    validation script converted to a module

--- extras/validation/validate_properties.py
+++ extras/validation/validate_properties.py
@@ -14,7 +14,8 @@ from _common_new import GaussianSourceKCSD3D
 
 
 class ValidateKESI(VerboseFFR):
-    def _eigensources(self, measurement_manager_basis, regularization_parameter=0):
+    def _eigensources(self, measurement_manager_basis,
+                      regularization_parameter=0):
         kernel = self.kernel
         cross_kernel = self.cross_kernel(measurement_manager_basis)
         self.eigenvalues, self.eigenvectors = self._evd(kernel,
@@ -46,53 +47,53 @@ def gaussian_source_factory_2d(xs, ys, sd, conductivity):
             for x, y in zip(xs, ys)]
 
 
-H = 3e-4
-standard_deviation = H*100 #H / 16
-conductivity = 0.3
-X, Y = np.mgrid[0.05: 0.95: 10j,
-                0.05: 0.95: 10j]
-ELECTRODES = pd.DataFrame({'X': X.flatten(),
-                           'Y': Y.flatten(),
-                           })
-forward = GaussianSourceKCSD3D(0, 0, 0, 3 * standard_deviation, conductivity)
+if __name__ == '__main__':
+    H = 3e-4
+    standard_deviation = H*100  # H / 16
+    conductivity = 0.3
+    X, Y = np.mgrid[0.05: 0.95: 10j,
+                    0.05: 0.95: 10j]
+    ELECTRODES = pd.DataFrame({'X': X.flatten(),
+                               'Y': Y.flatten(),
+                               })
 
-src_X, src_Y = np.mgrid[0.:1.:95j,
-                        0.:1.:95j]
+    measurement_manager = MeasurementManager(ELECTRODES)
+    src_X, src_Y = np.mgrid[0.:1.:95j,
+                            0.:1.:95j]
 
-sources = gaussian_source_factory_2d(src_X.flatten(),
-                                     src_Y.flatten(),
-                                     standard_deviation,
-                                     conductivity)
+    sources = gaussian_source_factory_2d(src_X.flatten(),
+                                         src_Y.flatten(),
+                                         standard_deviation,
+                                         conductivity)
 
-measurement_manager = MeasurementManager(ELECTRODES)
-reconstructor = ValidateKESI(sources, measurement_manager)
+    reconstructor = ValidateKESI(sources, measurement_manager)
 
-est_X, est_Y = np.mgrid[0.:1.:100j,
-                        0.:1.:100j]
-EST_POINTS = pd.DataFrame({'X': est_X.flatten(),
-                           'Y': est_Y.flatten(),
-                           })
-measurement_manager_basis = MeasurementManager(EST_POINTS)
-eigensources = reconstructor._eigensources(measurement_manager_basis)
+    est_X, est_Y = np.mgrid[0.:1.:100j,
+                            0.:1.:100j]
+    EST_POINTS = pd.DataFrame({'X': est_X.flatten(),
+                               'Y': est_Y.flatten(),
+                               })
+    measurement_manager_basis = MeasurementManager(EST_POINTS)
+    eigensources = reconstructor._eigensources(measurement_manager_basis)
 
-fig = plt.figure(figsize=(18, 16))
-heights = [1, 1, 1, 1]
+    fig = plt.figure(figsize=(18, 16))
+    heights = [1, 1, 1, 1]
 
-gs = gridspec.GridSpec(4, 4, height_ratios=heights, hspace=0.6, wspace=0.5)
-nr_plts = 16
+    gs = gridspec.GridSpec(4, 4, height_ratios=heights, hspace=0.6, wspace=0.5)
+    nr_plts = 16
 
-for i in range(nr_plts):
-    ax = fig.add_subplot(gs[i], aspect='equal')
+    for i in range(nr_plts):
+        ax = fig.add_subplot(gs[i], aspect='equal')
 
-    a = eigensources[:, i].reshape(len(est_X), len(est_X), 1)
-    cset = ax.contourf(est_X, est_Y, a[:, :, 0], cmap=cm.bwr)
+        a = eigensources[:, i].reshape(len(est_X), len(est_X), 1)
+        cset = ax.contourf(est_X, est_Y, a[:, :, 0], cmap=cm.bwr)
 
-    ax.text(0.5, 1.05, r"$\tilde{K} \cdot{v_{{%(i)d}}}$" % {'i': i+1},
-            horizontalalignment='center', transform=ax.transAxes,
-            fontsize=15)
+        ax.text(0.5, 1.05, r"$\tilde{K} \cdot{v_{{%(i)d}}}$" % {'i': i+1},
+                horizontalalignment='center', transform=ax.transAxes,
+                fontsize=15)
 
-a = reconstructor.eigenvalues
-plt.figure()
-plt.plot(a)
-plt.yscale('log')
-plt.show()
+    a = reconstructor.eigenvalues
+    plt.figure()
+    plt.plot(a)
+    plt.yscale('log')
+    plt.show()
