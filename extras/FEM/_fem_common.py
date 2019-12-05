3e5178c56f85867f10d1c9497915fecec4738792
Jakub Dzik <abukaj@users.noreply.github.com>
2019-12-05 17:05:12 +0100

    'fem_gaussian,py' refactored and extended
    `_SymmetricFEM_Base` superclass has been extracted from
    `GaussianPotentialFEM` class and moved to `_fem_common.py` module.
    The superclass consist of potentially reusable code.
    
    `GaussianSourceFactory` has been extended to support also linear
    interpolation and ground truth data (for diagnostic purposes).

---
+++ extras/FEM/_fem_common.py
@@ -0,0 +1,82 @@
+import datetime
+import logging
+
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+try:
+    from dolfin import (Constant, Mesh, MeshFunction, FunctionSpace,
+                        TestFunction, TrialFunction, Function,
+                        Measure, inner, grad, assemble, KrylovSolver,
+                        Expression, DirichletBC)
+
+except (ModuleNotFoundError, ImportError):
+    logger.warning("Unable to import from dolfin")
+
+else:
+    class _SymmetricFEM_Base(object):
+        RADIUS = 55.5
+        EXTERNAL_SURFACE = 1
+        MAX_ITER = 10000
+
+        def __init__(self, degree=3):
+            self._mesh = Mesh(self.PATH + '.xml')
+            self._subdomains = MeshFunction("size_t", self._mesh,
+                                            self.PATH + '_physical_region.xml')
+            self._boundaries = MeshFunction("size_t", self._mesh,
+                                            self.PATH + '_facet_region.xml')
+
+            self._change_degree(degree)
+
+        def _change_degree(self, degree):
+            self._V = FunctionSpace(self._mesh, "CG", degree)
+
+            self._dx = Measure("dx")(subdomain_data=self._subdomains)
+            self._v = TestFunction(self._V)
+            self._potential_function = Function(self._V)
+            self._potential_trial = TrialFunction(self._V)
+            self._a = inner(grad(self._potential_trial),
+                            grad(self._v)) * self._dx
+            self._terms_with_unknown = assemble(self._a)
+            self._degree = degree
+
+            self._solver = KrylovSolver("cg", "ilu")
+            self._solver.parameters["maximum_iterations"] = self.MAX_ITER
+            self._solver.parameters["absolute_tolerance"] = 1E-8
+
+        def __call__(self, degree, *args, **kwargs):
+            if degree != self._degree:
+                self._change_degree(degree)
+
+            csd = self._make_csd(degree, *args, **kwargs)
+            self.a = csd.a = (0.125 / assemble(csd
+                                               * Measure("dx", self._mesh)))
+            L = csd * self._v * self._dx
+            known_terms = assemble(L)
+
+            dirichlet_bc = DirichletBC(self._V,
+                                       Constant(
+                                                self._potential_behind_dome(
+                                                         self.RADIUS,
+                                                         *args, **kwargs)),
+                                       self._boundaries,
+                                       self.EXTERNAL_SURFACE)
+
+            terms_with_unknown = self._terms_with_unknown.copy()
+            dirichlet_bc.apply(terms_with_unknown, known_terms)
+
+            start = datetime.datetime.now()
+            try:
+                self.iterations = self._solver.solve(terms_with_unknown,
+                                                    self._potential_function.vector(),
+                                                    known_terms)
+                return self._potential_function
+
+            except RuntimeError as e:
+                self.iterations = self.MAX_ITER
+                logger.warning("Solver failed: {}".format(repr(e)))
+                return None
+
+            finally:
+                self.time = datetime.datetime.now() - start
--- extras/FEM/fem_gaussian.py
+++ extras/FEM/fem_gaussian.py
@@ -9,29 +9,33 @@ logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
 import numpy as np
-
-SOLUTION_FILENAME = os.path.join(os.path.dirname(__file__),
-                                 'Gaussian.npz')
-MAX_ITER = 10000
-
+from scipy.interpolate import RegularGridInterpolator
 
 class GaussianSourceFactory(object):
-    def __init__(self, standard_deviation=1., degree=3, _limit=np.inf):
+    SOLUTION_FILENAME = os.path.join(os.path.dirname(__file__),
+                                     'Gaussian.npz')
+
+    def __init__(self, standard_deviation=1., degree=3, _limit=np.inf,
+                 ground_truth=False):
         self.standard_deviation = standard_deviation
 
-        with np.load(SOLUTION_FILENAME) as fh:
+        with np.load(self.SOLUTION_FILENAME) as fh:
             N = min(_limit, fh['N'])
 
             self.a = fh['A_{:01.3f}_{}'.format(standard_deviation, degree)]
-            COMPRESSED = fh['Gaussian_SD_{:01.3f}_{}'.format(standard_deviation,
-                                                             degree)]
+            COMPRESSED = fh['Ground_truth_SD_{:01.3f}'.format(standard_deviation)
+                            if ground_truth else
+                            'Gaussian_SD_{:01.3f}_{}'.format(standard_deviation,
+                                                                         degree)]
 
             stride = 2 * N - 1
-            # POTENTIAL = self.empty([stride, stride, stride])
+
             self.POTENTIAL = self.empty(stride ** 3)
             self.X = self.empty(stride ** 3)
             self.Y = self.empty(stride ** 3)
             self.Z = self.empty(stride ** 3)
+
+            POTENTIAL = self.empty((stride, stride, stride))
 
             for x in range(N):
                 for y in range(x + 1):
@@ -52,17 +56,14 @@ class GaussianSourceFactory(object):
                                 self.Y[idx] = j
                                 self.Z[idx] = k
 
-                                    # POTENTIAL[N - 1 + i,
-                                    #           N - 1 + j,
-                                    #           N - 1 + k] = val
-        # X, Y, Z = np.meshgrid(np.arange(1 - N, 2 * N),
-        #                       np.arange(1 - N, 2 * N),
-        #                       np.arange(1 - N, 2 * N),
-        #                       indexing='ij')
-        # self.POTENTIAL = POTENTIAL.flatten()
-        # self.X = X.flatten()
-        # self.Y = Y.flatten()
-        # self.Z = Z.flatten()
+                                POTENTIAL[N - 1 + i,
+                                          N - 1 + j,
+                                          N - 1 + k] = val
+
+        self.interpolator = RegularGridInterpolator((np.linspace(1 - N, N - 1, stride),
+                                                     np.linspace(1 - N, N - 1, stride),
+                                                     np.linspace(1 - N, N - 1, stride)),
+                                                    POTENTIAL)
 
     def empty(self, shape):
         X = np.empty(shape)
@@ -76,11 +77,17 @@ class GaussianSourceFactory(object):
                          + np.square(Z))
                       / self.standard_deviation ** 2) * self.a
 
-    def potential(self, X, Y, Z):
+    # TODO: handle cases of distant points
+    def potential_sinc(self, X, Y, Z):
         return np.inner((np.sinc(np.subtract.outer(X, self.X))
                          * np.sinc(np.subtract.outer(Y, self.Y))
                          * np.sinc(np.subtract.outer(Z, self.Z))),
                         self.POTENTIAL)
+
+    def potential_linear(self, X, Y, Z):
+        return self.interpolator(np.stack((X, Y, Z),
+                                          axis=-1))
+
 
     class _Source(object):
         def __init__(self,
@@ -103,11 +110,20 @@ class GaussianSourceFactory(object):
                                      (Z - self._z) / self._scale)
                     / self._scale ** 3)
 
-        def potential(self, X, Y, Z):
-            return (self._parent.potential((X - self._x) / self._scale,
-                                           (Y - self._y) / self._scale,
-                                           (Z - self._z) / self._scale)
+        def _normalize(self, f, X, Y, Z):
+            return (f((X - self._x) / self._scale,
+                      (Y - self._y) / self._scale,
+                      (Z - self._z) / self._scale)
                     / (self._scale * self._conductivity))
+
+        def potential_sinc(self, X, Y, Z):
+            return self._normalize(self._parent.potential_sinc, X, Y, Z)
+
+        def potential(self, X, Y, Z):
+            return self.potential_sinc(X, Y, Z)
+
+        def potential_linear(self, X, Y, Z):
+            return self._normalize(self._parent.potential_linear, X, Y, Z)
 
     def Source(self, x=0, y=0, z=0, standard_deviation=1, conductivity=1):
         return self._Source(standard_deviation / self.standard_deviation,
@@ -116,14 +132,11 @@ class GaussianSourceFactory(object):
                             self)
 
 
-
 if __name__ == '__main__':
-    import datetime
     from scipy.special import erf
 
     try:
-        from dolfin import Constant, Mesh, MeshFunction, FunctionSpace, TestFunction, TrialFunction, Function, Measure, inner, grad, assemble, KrylovSolver
-        from dolfin import Expression, DirichletBC
+        from dolfin import Expression
 
     except (ModuleNotFoundError, ImportError):
         print("""Run docker first:
@@ -131,71 +144,74 @@ if __name__ == '__main__':
         $ cd /home/fenics/shared/
         """)
     else:
-        class GaussianPotentialFEM(object):
+        from _fem_common import _SymmetricFEM_Base
+
+        class GaussianPotentialFEM(_SymmetricFEM_Base):
             PATH = 'meshes/eighth_of_sphere'
-            EXTERNAL_SURFACE = 1
-            RADIUS = 55.5
 
-            def __init__(self):
-                self._mesh = Mesh(self.PATH + '.xml')
-                self._subdomains = MeshFunction("size_t", self._mesh,
-                                                self.PATH + '_physical_region.xml')
-                self._boundaries = MeshFunction("size_t", self._mesh,
-                                                self.PATH + '_facet_region.xml')
-
-            def __call__(self,
-                         standard_deviation=1.,
-                         degree=3):
-                V = FunctionSpace(self._mesh, "CG", degree)
-                v = TestFunction(V)
-                potential_trial = TrialFunction(V)
-                potential = Function(V)
-                dx = Measure("dx")(subdomain_data=self._subdomains)
-                # ds = Measure("ds")(subdomain_data=self._boundaries)
-                csd = Expression(f'''
+            def _make_csd(self, degree, standard_deviation):
+                return Expression(f'''
                                  a * exp({-0.5 / standard_deviation ** 2}
                                          * ((x[0])*(x[0])
                                             + (x[1])*(x[1])
                                             + (x[2])*(x[2])
                                             ))
                                  ''',
-                                 degree=degree,
-                                 a=1.0)
-                self.a = csd.a = (0.125
-                                  / assemble(csd * Measure("dx", self._mesh)))
-                # print(assemble(csd * Measure("dx", self._mesh)))
-                L = csd * v * dx
-                known_terms = assemble(L)
-                a = inner(grad(potential_trial), grad(v)) * dx
-                terms_with_unknown = assemble(a)
-                dirchlet_bc = DirichletBC(V,
-                                          Constant(0.25
-                                                   * erf(self.RADIUS
-                                                         / (np.sqrt(2)
-                                                            * standard_deviation))
-                                                   / (self.RADIUS * np.pi)),
-                                          self._boundaries,
-                                          self.EXTERNAL_SURFACE)
-                dirchlet_bc.apply(terms_with_unknown, known_terms)
-                solver = KrylovSolver("cg", "ilu")
-                solver.parameters["maximum_iterations"] = MAX_ITER
-                solver.parameters["absolute_tolerance"] = 1E-8
-                # solver.parameters["monitor_convergence"] = True
-                start = datetime.datetime.now()
-                try:
-                    self.iterations = solver.solve(terms_with_unknown,
-                                                   potential.vector(),
-                                                   known_terms)
-                    return potential
+                                  degree=degree,
+                                  a=1.0)
 
-                except RuntimeError as e:
-                    self.iterations = MAX_ITER
-                    logger.warning("Solver failed: {}".format(repr(e)))
-                    return None
+            def _potential_behind_dome(self, radius, standard_deviation):
+                return (0.25
+                        * erf(radius
+                              / (np.sqrt(2)
+                                 * standard_deviation))
+                        / (radius * np.pi))
 
-                finally:
-                    self.time = datetime.datetime.now() - start
 
+### COPIED FROM _common_new.py
+        class GaussianSourceBase(object):
+            def __init__(self, x, y, z, standard_deviation):
+                self.x = x
+                self.y = y
+                self.z = z
+                self._variance = standard_deviation ** 2
+                self._a = (2 * np.pi * self._variance) ** -1.5
+
+
+        class GaussianSourceKCSD3D(GaussianSourceBase):
+            _dtype = np.sqrt(0.5).__class__
+            _fraction_of_erf_to_x_limit_in_0 = _dtype(2 / np.sqrt(np.pi))
+            _x = _dtype(1.)
+            _half = _dtype(0.5)
+            _last = 2.
+            _err = 1.
+            while 0 < _err < _last:
+                _radius_of_erf_to_x_limit_applicability = _x
+                _last = _err
+                _x *= _half
+                _err = _fraction_of_erf_to_x_limit_in_0 - erf(_x) / _x
+
+            def __init__(self, x, y, z, standard_deviation, conductivity):
+                super(GaussianSourceKCSD3D, self).__init__(x, y, z,
+                                                           standard_deviation)
+                self.conductivity = conductivity
+                self._b = 0.25 / (np.pi * conductivity)
+                self._c = np.sqrt(0.5) / standard_deviation
+
+            def csd(self, X, Y, Z):
+                return self._a * np.exp(-0.5 * (
+                            (X - self.x) ** 2 + (Y - self.y) ** 2 + (
+                                Z - self.z) ** 2) / self._variance)
+
+            def potential(self, X, Y, Z):
+                R = np.sqrt(
+                    (X - self.x) ** 2 + (Y - self.y) ** 2 + (Z - self.z) ** 2)
+                Rc = R * self._c
+                return self._b * np.where(
+                    Rc >= self._radius_of_erf_to_x_limit_applicability,
+                    erf(Rc) / R,
+                    self._c * self._fraction_of_erf_to_x_limit_in_0)
+### END OF COPIED FROM _common_new.py
 
         logging.basicConfig(level=logging.INFO)
 
@@ -207,8 +223,9 @@ if __name__ == '__main__':
                    }
         for degree in [1, 2, 3]:
             for sd in [0.25, 0.5, 1, 2]:
+                ground_truth = GaussianSourceKCSD3D(0, 0, 0, sd, 1)
                 logger.info('Gaussian SD={} (deg={})'.format(sd, degree))
-                potential = fem(sd, degree)
+                potential = fem(degree, sd)
 
                 stats.append((sd,
                               degree,
@@ -220,12 +237,16 @@ if __name__ == '__main__':
                 if potential is not None:
                     POTENTIAL = np.empty(N * (N + 1) * (N + 2) // 6)
                     POTENTIAL.fill(np.nan)
+                    GT = POTENTIAL.copy()
                     for x in range(N):
                         for y in range(x + 1):
                             for z in range(y + 1):
-                                POTENTIAL[x * (x + 1) * (x + 2) // 6
-                                          + y * (y + 1) // 2
-                                          + z] = potential(x, y, z)
+                                idx = x * (x + 1) * (x + 2) // 6 + y * (
+                                            y + 1) // 2 + z
+                                POTENTIAL[idx] = potential(x, y, z)
+                                GT[idx] = ground_truth.potential(x, y, z)
                     results['Gaussian_SD_{:01.3f}_{}'.format(sd, degree)] = POTENTIAL
+                    results['Ground_truth_SD_{:01.3f}'.format(sd)] = GT
                     results['A_{:01.3f}_{}'.format(sd, degree)] = fem.a
-                    np.savez_compressed(SOLUTION_FILENAME, **results)
+                    np.savez_compressed(GaussianSourceFactory.SOLUTION_FILENAME,
+                                        **results)
