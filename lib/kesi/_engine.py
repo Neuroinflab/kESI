fa0ea76dc876c6f202765e248c81fa86e31793f1
Jakub Dzik <abukaj@users.noreply.github.com>
2020-04-15 19:17:12 +0200

    Update copyright information

--- lib/kesi/_engine.py
+++ lib/kesi/_engine.py
@@ -4,7 +4,7 @@
 #                                                                             #
 #    kESI                                                                     #
 #                                                                             #
-#    Copyright (C) 2019 Jakub M. Dzik (Laboratory of Neuroinformatics;        #
+#    Copyright (C) 2019-2020 Jakub M. Dzik (Laboratory of Neuroinformatics;   #
 #    Nencki Institute of Experimental Biology of Polish Academy of Sciences)  #
 #                                                                             #
 #    This software is free software: you can redistribute it and/or modify    #
