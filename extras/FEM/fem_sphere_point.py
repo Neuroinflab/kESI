658de2fd6ee396d13d30ba33ee0160a03c8a48b7
Jakub Dzik <abukaj@users.noreply.github.com>
2020-04-25 21:28:41 +0200

    Add debug code

--- extras/FEM/fem_sphere_point.py
+++ extras/FEM/fem_sphere_point.py
@@ -465,7 +465,13 @@ class _SomeSpherePointController2D(_PointLoaderBase2D,
             fem = self._fem
 
         self._anything_new = True
-        idx_r = np.where(np.isclose(self.R, r))[0][0]
+        try:
+            idx_r = np.where(np.isclose(self.R, r))[0][0]
+        except IndexError:
+            print(self.R)
+            print(r)
+            print(self.R - r)
+            raise
 
         sampling_time = np.nan
         if potential is not None:
