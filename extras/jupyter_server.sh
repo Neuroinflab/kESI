a89884136524319f44f65eb87a1671bcd3947d05
Jakub M. Dzik <abukaj@users.noreply.github.com>
2021-07-30 11:45:57 +0200

    Enable debugging of Jupyter notebook

--- extras/jupyter_server.sh
+++ extras/jupyter_server.sh
@@ -6,4 +6,4 @@
 # $ openssl req -x509 -nodes -days 365 -newkey rsa:4096 -keyout .jupyter_nb.key -out .jupyter_nb.pem
 # $ conda create -n jupyter --no-default-packages ipython jupyter nbpresent nb_conda nb_conda_kernels
 conda activate jupyter
-jupyter notebook --no-browser --certfile .jupyter_nb.pem --keyfile .jupyter_nb.key --ip $(hostname) --port 8888
+jupyter notebook --debug --no-browser --certfile .jupyter_nb.pem --keyfile .jupyter_nb.key --ip $(hostname) --port 8888
