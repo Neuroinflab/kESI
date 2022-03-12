443276aecb382303590e0a0fe2f210dccf27b313
Jakub M. Dzik <abukaj@users.noreply.github.com>
2022-03-12 22:21:21 +0100

    Include slice geometries into Snakemake pipeline
    Slice geometries were implemented before development of the Snakemake
    pipeline thus originally not included into the pipeline.  For the sake
    of reproducibility and uniformity, they required to be integrated with
    the Snakefile.

---
+++ extras/paper_sample_slice_solution.py
@@ -0,0 +1,111 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import argparse
+import datetime
+import configparser
+
+import numpy as np
+
+import FEM.fem_slice_point_new as fspn
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Sample slice FEM solutions.')
+    parser.add_argument('-o', '--output',
+                        metavar='<output.npz>',
+                        dest='output',
+                        help='path to the output file',
+                        nargs='?')
+    parser.add_argument('-c', '--config',
+                        metavar='<config.ini>',
+                        dest='config',
+                        help='path to the FEM config file',
+                        nargs='?')
+    parser.add_argument('-n', '--name',
+                        metavar='<solution name>',
+                        dest='name',
+                        help='name of the solution',
+                        nargs='?')
+    parser.add_argument('-q', '--quiet',
+                        dest='quiet',
+                        action='store_true',
+                        help='do not print results',
+                        default=False)
+    parser.add_argument('-k',
+                        type=int,
+                        dest='k',
+                        metavar="<k>",
+                        help="each dimansion is sampled in 2**k + 1 points",
+                        default=9)
+    parser.add_argument('-r', '--sampling_radius',
+                        type=float,
+                        dest='radius',
+                        metavar="<radius>",
+                        help='length of the sampled cube side [m]',
+                        default=3e-4)
+    parser.add_argument('-f', '--fill',
+                        type=float,
+                        dest='fill',
+                        metavar="<fill value>",
+                        help='fill value',
+                        default=np.nan)
+
+    args = parser.parse_args()
+
+
+    N = 2 ** args.k + 1
+    X = np.linspace(-args.radius / 2, args.radius / 2, N)
+    Y = np.linspace(-args.radius / 2, args.radius / 2, N)
+    Z = np.linspace(0, args.radius, N)
+
+    loading_start = datetime.datetime.now()
+    fem = fspn.SlicePointSourcePotentialFEM(args.config)
+    preprocess_start = datetime.datetime.now()
+
+    try:
+        print(args.name)
+        correction_potential = fem._fm.load(args.name)
+    except configparser.NoSectionError:
+        print(' SOLUTION NOT FOUND')
+        exit(-1)
+
+    CORRECTION_POTENTIAL = np.full((len(X),
+                                    len(Y),
+                                    len(Z)),
+                                   args.fill)
+
+    ERROR_R = []
+    start = datetime.datetime.now()
+    if not args.quiet:
+        print(f'{args.name}\tLOADING: {preprocess_start - loading_start}')
+        print(f'{args.name}\tPREPROCESSING: {start - preprocess_start}')
+    for i_x, x in enumerate(X):
+        if not args.quiet:
+            print(
+                f'{args.name}\t{i_x}\t{x * 1000_000:.1f}um\t{datetime.datetime.now() - start}')
+
+        for i_y, y in enumerate(Y):
+            for i_z, z in enumerate(Z):
+                try:
+                    CORRECTION_POTENTIAL[i_x, i_y, i_z] = correction_potential(x, y, z)
+                except RuntimeError:
+                    ERROR_R.append((x, y, z))
+
+    np.savez_compressed(args.output,
+                        CORRECTION_POTENTIAL=CORRECTION_POTENTIAL,
+                        X=X,
+                        Y=Y,
+                        Z=Z,
+                        LOCATION=[fem._fm.getfloat(args.name, c) for c in
+                                  'xyz'],
+                        BASE_CONDUCTIVITY=fem._fm.getfloat(args.name,
+                                                           'base_conductivity'),
+                        _PREPROCESSING_TIME=(
+                                start - preprocess_start).total_seconds(),
+                        _PROCESSING_TIME=(
+                                datetime.datetime.now() - start).total_seconds())
+
+    if ERROR_R != []:
+        print('ERROR R:')
+        print(ERROR_R)
---
+++ extras/paper_solve_slice_on_plate.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import argparse
+import configparser
+
+import FEM.fem_slice_point_new as fspn
+import FEM.fem_common as fc
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Solve slice FEM solutions.')
+    parser.add_argument('-o', '--output',
+                        metavar='<metadata.ini>',
+                        dest='output',
+                        help='path to the metadata file',
+                        nargs='?')
+    parser.add_argument('-c', '--config',
+                        metavar='<config.ini>',
+                        dest='config',
+                        help='path to the FEM config file',
+                        nargs='?')
+    parser.add_argument('-n', '--name',
+                        metavar='<solution name>',
+                        dest='name',
+                        help='name of the solution',
+                        nargs='?')
+    parser.add_argument('-q', '--quiet',
+                        dest='quiet',
+                        action='store_true',
+                        help='do not print results',
+                        default=False)
+
+    args = parser.parse_args()
+
+    setup_time = fc.fc.Stopwatch()
+    total_solving_time = fc.fc.Stopwatch()
+    with setup_time:
+        fem = fspn.SlicePointSourcePotentialFEM(args.config)
+
+    name = args.name
+    ex, ey, ez = [fem._fm.getfloat(name, a) for a in 'xyz']
+    
+    conductivity = fem.base_conductivity(ex, ey, ez)
+
+    if not args.quiet:
+        print(' solving')
+
+    potential_corr = fem.correction_potential(ex, ey, ez)
+    metadata = {'filename': fem._fm.get(name, 'filename'),
+          'x': ex,
+          'y': ey,
+          'z': ez,
+          'setup_time': float(setup_time),
+          'total_solving_time': float(total_solving_time),
+          'local_preprocessing_time': float(
+              fem.local_preprocessing_time),
+          'solving_time': float(fem.solving_time),
+          'base_conductivity': conductivity,
+                }
+    fem._fm.store(name, potential_corr,
+                  metadata)
+
+    config = configparser.ConfigParser()
+    config.add_section(name)
+    for k, v in metadata.items():
+        config.set(name, k, v if isinstance(v, str) else repr(v))
+    config.write(open(args.output, 'w'))
