87d59f6be0c762b11ef964a88dae851394603dc6
Jakub M. Dzik <abukaj@users.noreply.github.com>
2022-04-21 14:33:51 +0200

    Update requirements for the Jupyter server
    Package installation order started to matter.

--- extras/jupyter_server.sh
+++ extras/jupyter_server.sh
@@ -4,6 +4,10 @@
 #
 # REQUIRES:
 # $ openssl req -x509 -nodes -days 365 -newkey rsa:4096 -keyout .jupyter_nb.key -out .jupyter_nb.pem
-# $ conda create -n jupyter --no-default-packages ipython jupyter nbpresent nb_conda nb_conda_kernels
+# $ conda create -n jupyter --no-default-packages ipython jupyter
+# $ conda activate jupyter
+# $ conda install nb_conda nb_conda_kernels
+# $ conda install nbpresent
+# $ conda deactivate
 conda activate jupyter
 jupyter notebook --debug --no-browser --certfile .jupyter_nb.pem --keyfile .jupyter_nb.key --ip $(hostname) --port 8888
