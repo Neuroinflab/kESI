c9165a1cf13b4ee33afc45cf39f2ec169d06a762
Jakub M. Dzik <abukaj@users.noreply.github.com>
2022-08-12 18:52:48 +0200

    Fix the Mamba installation issues (hopefully)

--- setup_conda_envs.sh
+++ setup_conda_envs.sh
@@ -1,14 +1,12 @@
 # !!! IMPORTANT !!!
 # run with `source`
 # 3.5 -> no gcc package required by fenics
-conda install -c conda-forge --yes mamba
 for python_version in "3.6" "3.7" "3.8"
 do
   name=kesi${python_version/./}
-  mamba create --name $name --no-default-packages --yes -c conda-forge -c bioconda snakemake python=$python_version
-## there is at least one system in which mamba installed in the base environment is not available in other environments
-#  mamba create --name $name --no-default-packages --yes -c conda-forge -c bioconda snakemake mamba python=$python_version
+  conda create --name $name --no-default-packages --yes -c conda-forge mamba python=$python_version
   conda activate $name
+  mamba install -c bioconda --yes snakemake
   mamba install -c conda-forge --yes numpy scipy matplotlib pandas ipython ipykernel importlib_metadata
 ## there is at least one system in which ipython_genutils are missing if not installed explicitely
 #  mamba install -c conda-forge --yes numpy scipy matplotlib pandas ipython ipython_genutils ipykernel importlib_metadata
