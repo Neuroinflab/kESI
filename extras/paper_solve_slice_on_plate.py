3c7487b5b15ca568d39d5a94530004bdf901bea2
Jakub M. Dzik <abukaj@users.noreply.github.com>
2022-08-17 18:16:33 +0200

    Fix context manager scope issue
    Amends 97fbb0cf713efe725c8a680a700445f8839a8898

--- extras/paper_solve_slice_on_plate.py
+++ extras/paper_solve_slice_on_plate.py
@@ -76,40 +76,40 @@ if __name__ == '__main__':
         metadata.set('fem', 'element_type', args.element_type)
         metadata.setpath('model', 'config', args.config)
 
-    with setup_time:
-        function_manager = fc.FunctionManager(args.mesh,
-                                              args.degree,
-                                              args.element_type)
-        fem = fspn.SlicePointSourcePotentialFEM(function_manager,
-                                                args.config)
+        with setup_time:
+            function_manager = fc.FunctionManager(args.mesh,
+                                                  args.degree,
+                                                  args.element_type)
+            fem = fspn.SlicePointSourcePotentialFEM(function_manager,
+                                                    args.config)
 
-        metadata.set('correction',
-                     'global_preprocessing_time',
-                     float(fem.global_preprocessing_time))
+            metadata.set('correction',
+                         'global_preprocessing_time',
+                         float(fem.global_preprocessing_time))
 
-    metadata.set('correction', 'setup_time', float(setup_time))
+        metadata.set('correction', 'setup_time', float(setup_time))
 
-    config = configparser.ConfigParser()
-    config.read(args.electrodes)
-    electrode_coords = [config.getfloat(args.name, a) for a in 'xyz']
-    for k, v in zip('xyz', electrode_coords):
-        metadata.set('electrode', k, v)
+        config = configparser.ConfigParser()
+        config.read(args.electrodes)
+        electrode_coords = [config.getfloat(args.name, a) for a in 'xyz']
+        for k, v in zip('xyz', electrode_coords):
+            metadata.set('electrode', k, v)
 
-    if not args.quiet:
-        print(' solving')
+        if not args.quiet:
+            print(' solving')
 
-    with total_solving_time:
-        potential_corr = fem.correction_potential(*electrode_coords)
+        with total_solving_time:
+            potential_corr = fem.correction_potential(*electrode_coords)
 
-    metadata.setfields(
-        'correction',
-        {
-          'total_solving_time': float(total_solving_time),
-          'local_preprocessing_time': float(fem.local_preprocessing_time),
-          'solving_time': float(fem.solving_time),
-          'base_conductivity': fem.base_conductivity(*electrode_coords),
-        })
+        metadata.setfields(
+            'correction',
+            {
+              'total_solving_time': float(total_solving_time),
+              'local_preprocessing_time': float(fem.local_preprocessing_time),
+              'solving_time': float(fem.solving_time),
+              'base_conductivity': fem.base_conductivity(*electrode_coords),
+            })
 
-    function_manager.store(function_filename,
-                           potential_corr)
-    metadata.setpath('correction', 'filename', function_filename)
+        function_manager.store(function_filename,
+                               potential_corr)
+        metadata.setpath('correction', 'filename', function_filename)
