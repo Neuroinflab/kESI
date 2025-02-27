284b9220daed640c2e94b739fdcc1420196f2ef2
Marian Dovgialo <m.dovgialo@nencki.edu.pl>
2025-02-27 16:46:43 +0100

    working MRI to mesh with electrode embedding and additional boundaries, MFEM fails to load when no electrode has been added???

--- src/kesi/fem_utils/embed_a_ground_electrode.py
+++ src/kesi/fem_utils/embed_a_ground_electrode.py
@@ -1,23 +1,89 @@
-# TODO FINISH THIS ONE DAY, SPLIT MESH BY MATERIAL CREATE AN ELECTRODE AND EMBED IT USING GMSH ...
-from collections import defaultdict
 
-import gmsh
 import numpy as np
 import pyvista
+
+# todo
+# if there a big mismatch, clip is acting weirdly and unacurately
+# if the electrode is smalelr than voxel, it won't cut anything using clip...
+
 brain_path = "/home/mdovgialo/projects/halje_data_analysis/kESI/extras/data/generated/meshes/fsaverage/fsaverage_brain_materials_2mm.nii_volume.vtk"
+brain_mesh = pyvista.read(brain_path)
+
+extra_boundaries = (0.15, 0.01, 0.5, 0.05, 10.0, 1.0, 200.0, 10.0)
+extra_boundaries = (0.15, 0.01, 0.5, 0.05, 10.0, 1.0)
+extra_boundaries = (0.05, 0.01, 0.10, 0.02, 0.20, 0.03)
+merge_tolerance = 0.00001
+electrode_coord = (0.001, -0.122, -0.005)
+electrode_coord = (0.0, 0.0,0.0)
+electrode_radius = 0.001
+
+average_cell_size =  brain_mesh.get_cell(0).cast_to_unstructured_grid().compute_cell_sizes()["Volume"][0] ** (1/3)
+
+if electrode_radius < average_cell_size:
+    electrode_radius = average_cell_size
+electrode = pyvista.Sphere(radius=electrode_radius, center=electrode_coord)
+electrode.cell_data["material"] = np.array([10] * electrode.n_cells, dtype=brain_mesh.cell_data.active_scalars.dtype)
+brain_with_electrode = brain_mesh.clip_surface(electrode, invert=False, progress_bar=True, crinkle=False)
 
 
-gmsh.initialize()
-gmsh.model.add("brain")
-gmsh.merge(brain_path)
-pv_mesh = pyvista.read(brain_path)
-cell_data = pv_mesh.cell_data['material']
+boundary_sizes = extra_boundaries[::2]
+boundary_resolutions = extra_boundaries[1::2]
+boundaries = list(reversed(sorted([[i, j] for i, j in zip(boundary_sizes, boundary_resolutions)])))
 
-unique_material_ids = np.unique(cell_data)
+boundary_meshes = []
+for size, res in boundaries:
+    print(size, res)
+    coords = np.arange(-size, size, res)
+    x, y, z = np.meshgrid(coords, coords, coords, indexing='ij')
+    boundary = pyvista.UnstructuredGrid(pyvista.StructuredGrid(x, y, z))
+    boundary.cell_data['material'] = np.array([np.max(np.unique(brain_with_electrode.cell_data['material']))] * boundary.n_cells, dtype = brain_with_electrode.cell_data['material'].dtype)
+    boundary_meshes.append(boundary)
 
-for material_id in sorted(unique_material_ids):
-    # Find the cell tags corresponding to the current material ID
-    cell_tags = [i + 1 for i, tag in enumerate(cell_data) if tag == material_id]  # +1 to adjust for GMSH's 1-based indexing
 
-    # Create a physical group for this material
-    gmsh.model.addPhysicalGroup(3, cell_tags, name=f"Material_{material_id}")  # Change dimension if necessary
+meshes_to_merge = boundary_meshes + [brain_with_electrode, ]
+meshes_to_merge = boundary_meshes
+
+## TODO
+# this makes invalid topology!!!
+final_mesh = None
+for current in meshes_to_merge:
+    if final_mesh is None:
+        final_mesh = current
+        continue
+    surface = current.extract_surface()
+#     clipped = final_mesh.clip_surface(surface, invert=False, progress_bar=True)
+    clipped = final_mesh.clip_box(current.bounds, invert=True, progress_bar=True)
+    final_mesh = clipped.merge(current, merge_points=True, tolerance=merge_tolerance, inplace=False, progress_bar=True)
+# final_mesh = final_mesh.clean(tolerance=merge_tolerance, progress_bar=True)
+meshes_to_merge[0].save("/home/mdovgialo/testing_electrode_embedding/weird_boundary.vtu", binary=False)
+final_mesh.save("/home/mdovgialo/testing_electrode_embedding/weird_boundary.vtu", binary=False)
+
+
+
+
+
+coords_bigger = np.arange(-200, 200, 0.5)
+boundary_bigger = pyvista.StructuredGrid(coords_bigger, coords_bigger, coords_bigger)
+
+
+coords_big = np.arange(-0.5, 0.5, 0.020)
+boundary_big = pyvista.StructuredGrid(coords_big, coords_big, coords_big)
+
+
+
+
+import pyvista
+
+# todo
+# if there a big mismatch, clip is acting weirdly and unacurately
+# if the electrode is smalelr than voxel, it won't cut anything using clip...
+
+brain_path = "/home/mdovgialo/projects/halje_data_analysis/kESI/extras/data/generated/meshes/fsaverage/fsaverage_brain_materials_2mm.nii_volume.vtk"
+brain_mesh = pyvista.read(brain_path)
+
+clipped = brain_mesh.clip_box((-0.03, 0.03, -0.03, 0.03,-0.03, 0.03), invert=True)
+clipped_tesselated = clipped.tessellate(2)
+brain_mesh_clipped = brain_mesh.clip_box((-0.03, 0.03, -0.03, 0.03,-0.03, 0.03), invert=False)
+merged = brain_mesh_clipped.merge(clipped_tesselated, tolerance=merge_tolerance)
+
+
--- src/kesi/fem_utils/mri_partitioning_to_mesh.py
+++ src/kesi/fem_utils/mri_partitioning_to_mesh.py
@@ -1,3 +1,5 @@
+import warnings
+
 import nibabel
 from nibabel.affines import apply_affine
 import numpy as np
@@ -6,6 +8,7 @@ import os
 import argparse
 import pyvista
 
+from kesi.utils import write_run_summary
 from kesi.fem_utils.grid_utils import vertex_grid_from_volume
 import vtk
 from tqdm import tqdm
@@ -14,38 +17,81 @@ import mfem.ser as mfem
 from sklearn.neighbors import KDTree
 from nibabel.processing import conform
 
-def create_cell_data_from_mri(mri, mri_grid):
+
+def create_cell_data_from_mri(mri, mri_grid, default_value):
     """assumes mri is in mm and mri_grid is in meters"""
     cell_centers = mri_grid.cell_centers().points * 1000
     inv_affine = np.linalg.inv(mri.affine)
-    voxel_ids = apply_affine(inv_affine, cell_centers).astype(int)
+    voxel_ids = apply_affine(inv_affine, cell_centers).round().astype(int)
     mri_data = mri.get_fdata()
     data = []
-    for i in tqdm(voxel_ids, desc='sorting data'):
-        data.append(mri_data[i[0], i[1], i[2]])
+    for i in tqdm(voxel_ids, desc='assigning materials to cells'):
+        try:
+            if i[0] < 0 or i[1] < 0 or i[2] < 0:
+                data.append(default_value)
+            else:
+                data.append(mri_data[i[0], i[1], i[2]])
+        except IndexError:
+            data.append(default_value)
     return data
+
 
 def align_mri_volume_to_ras(mri):
     voxel_sizes = tuple(np.abs([mri.header.get_base_affine()[0][0],
-                   mri.header.get_base_affine()[1][1],
-                   mri.header.get_base_affine()[2][2],
-                   ]))
-    mri = conform(mri, out_shape=mri.shape, voxel_size=voxel_sizes , order=0)
+                                mri.header.get_base_affine()[1][1],
+                                mri.header.get_base_affine()[2][2],
+                                ]))
+    if len(mri.shape) > 3:
+        mri = nibabel.funcs.squeeze_image(mri)
+    mri = conform(mri, out_shape=mri.shape, voxel_size=voxel_sizes, order=0)
     return mri
+
 
 def main():
     parser = argparse.ArgumentParser(description=("A tool to transform partitioned MRI scan to a cube mesh "
                                                   "with materials. Assumes each voxel is marked with material index. "
                                                   "Saves MFEM compatible messh with the same name."
-                                                  " Will safe mfem .mesh file with materials and grounding plate boundary defined. "
-                                                  "Optionally, can save a .vtk with mesh materials and .vtu with mesh boundaries."
-                                                  "For visualisation in applications which cannot load MFEM format"))
+                                                  ""))
     parser.add_argument("mri",
                         help=('Segmented 3D volume file, for example .nii.gz format'))
-    parser.add_argument("-o" "--outdir",
+    parser.add_argument("-o", "--outdir",
                         help='output directory')
+    parser.add_argument("-b", "--boundaries", nargs='+', type=float,
+                        help=("extra boundaries consisting of last material,"
+                              " list of size and resolution. "
+                              "For  example: 0.15 0.01 0.5 0.05. For"
+                              " boundaries with decreasing spatial resolution"),
+                        # default=(0.5, 0.3, 200.0, 50.0))
+                        default=(0.5, 0.2, 10.0, 3.0, 200.0, 10.0))
+                        # default=(0.15, 0.03, 0.5, 0.2, 10.0, 3.0,))
+    parser.add_argument("-e", "--electrode", nargs=3, type=float,
+                        help=("grounding electrode position if not given there will be only far boundary condition"),
+                        default=None)
+    parser.add_argument("-r", "--electrode-radius", type=float,
+                        help=("Grounding electrode radius"),
+                        default=0.002)
+    parser.add_argument('--crinkle', action=argparse.BooleanOptionalAction,
+                        help=("Instead of carving out the electrode"
+                              " shape in the mesh, delete a few mesh cells")
+                        )
 
     namespace = parser.parse_args()
+    os.makedirs(namespace.outdir, exist_ok=True)
+    write_run_summary(namespace.outdir, namespace)
+
+
+    # extra_boundaries = (0.15, 0.01, 0.5, 0.05, 10.0, 1.0, 200.0, 10.0)
+    # extra_boundaries = (0.15, 0.01, 0.5, 0.05, 10.0, 1.0, 30, 10.0)
+    extra_boundaries = namespace.boundaries
+    electrode_radius = namespace.electrode_radius
+    electrode_position = namespace.electrode
+
+    assert (len(extra_boundaries) % 2) == 0
+    # # extra_boundaries = (0.05, 0.01, 0.10, 0.02, 0.20, 0.03)
+    # merge_tolerance = 0.00001
+    # electrode_coord = (0.001, -0.12, -0.005)
+    # electrode_coord = (0.0, 0.0, 0.0)
+    # electrode_radius = 0.003
 
     base_outfile = os.path.splitext(namespace.mri)[0]
 
@@ -56,55 +102,76 @@ def main():
     meshgrid = vertex_grid_from_volume(mri)
 
     mri_grid = pyvista.StructuredGrid(meshgrid[0], meshgrid[1], meshgrid[2])
-    cell_data = create_cell_data_from_mri(mri, mri_grid)
 
-    mri_grid.cell_data['material'] = np.array(cell_data, dtype=int)
+    mri_x = meshgrid[0][:, 0, 0]
+    mri_y = meshgrid[1][0, :, 0]
+    mri_z = meshgrid[2][0, 0, :]
 
-    mri_unstructured_grid = pyvista.UnstructuredGrid(mri_grid)
+    boundary_sizes = extra_boundaries[::2]
+    boundary_resolutions = extra_boundaries[1::2]
+    boundaries = list(sorted([[i, j] for i, j in zip(boundary_sizes, boundary_resolutions)]))
 
-    surf = mri_unstructured_grid.extract_surface(progress_bar=True)
-    surf.compute_normals()
+    boundary_coords = [mri_x, mri_y, mri_z]
+    for b in boundaries:
+        b_x = np.arange(-b[0]/2, b[0]/2 + b[1], b[1])
+        b_x = b_x[np.logical_or(b_x < np.min(boundary_coords[0]), b_x > np.max(boundary_coords[0]))]
+        final_x = np.array(sorted(list(boundary_coords[0]) + list(b_x)))
 
-    down_cell_positions = surf.cell_centers().points[surf.face_normals[:, 2] == -1]
-    boundary_tree = KDTree(down_cell_positions)
+        b_y = np.arange(-b[0]/2, b[0]/2 + b[1], b[1])
+        b_y = b_y[np.logical_or(b_y < np.min(boundary_coords[1]), b_y > np.max(boundary_coords[1]))]
+        final_y = np.array(sorted(list(boundary_coords[1]) + list(b_y)))
 
-    with tempfile.NamedTemporaryFile(suffix='.vtk') as fp:
-        writer = vtk.vtkUnstructuredGridWriter()
-        writer.SetFileName(fp.name)
-        writer.SetInputData(mri_unstructured_grid)
-        writer.SetFileVersion(42)
-        writer.Write()
-
-        mfem_mesh = mfem.Mesh(fp.name)
-        boundary_elements_coords = []
-
-        for i in range(mfem_mesh.GetNBE()):
-            vertices = mfem_mesh.GetBdrElementVertices(i)
-            # todo look how boundary vertices look like, what are they, can I mark any as boundary?
-            coords = np.array([mfem_mesh.GetVertexArray(j) for j in vertices])
-            mean_coords = coords.mean(axis=0)
-            boundary_elements_coords.append(mean_coords)
-        boundary_elements_coords = np.array(boundary_elements_coords)
+        b_z = np.arange(-b[0]/2, b[0]/2 + b[1], b[1])
+        b_z = b_z[np.logical_or(b_z < np.min(boundary_coords[2]), b_z > np.max(boundary_coords[2]))]
+        final_z = np.array(sorted(list(boundary_coords[2]) + list(b_z)))
+        boundary_coords = [final_x, final_y, final_z]
 
 
-        vox_size = np.mean(np.abs([mri.header.get_base_affine()[0][0],
-                        mri.header.get_base_affine()[1][1],
-                        mri.header.get_base_affine()[2][2],
-                               ]
-                              )
-                       ) / 1000 # in meters
-        counts = boundary_tree.query_radius(boundary_elements_coords, r=vox_size * 1.5, count_only=True)
 
-        for i in range(mfem_mesh.GetNBE()):
-            if counts[i] > 0:
-                mfem_mesh.SetBdrAttribute(i, 2)
-            else:
-                mfem_mesh.SetBdrAttribute(i, 1)
-        mfem_mesh.FinalizeMesh(0, True)
-        mfem_mesh.Print(base_outfile + '.mesh')
-        # todo add save vtk param
-        mfem_mesh.PrintVTK(base_outfile + '_volume.vtk')
-        mfem_mesh.PrintBdrVTU(base_outfile + '_boundaries')
+    mri_with_boundaries = pyvista.RectilinearGrid(*boundary_coords)
+    mri_with_boundaries = mri_with_boundaries.cast_to_unstructured_grid()
+
+    # default value - last material
+    cell_data = create_cell_data_from_mri(mri, mri_with_boundaries,
+                                          list(reversed(sorted(np.unique(mri.get_fdata()))))[0]
+                                          )
+
+    mri_with_boundaries.cell_data['material'] = np.array(cell_data, dtype=int)
+
+    if electrode_position is not None:
+        average_cell_size = mri_grid.get_cell(0).cast_to_unstructured_grid().compute_cell_sizes()["Volume"][0] ** (
+                1 / 3)
+
+        if electrode_radius < average_cell_size:
+            warnings.warn("Electrode radius {} too small. Increasing electrode radius to {}".format(electrode_radius,
+                                                                                                    average_cell_size))
+            electrode_radius = average_cell_size
+
+        electrode = pyvista.Sphere(radius=electrode_radius, center=electrode_position)
+        electrode.cell_data["material"] = np.array([10] * electrode.n_cells,
+                                                   dtype=mri_with_boundaries.cell_data.active_scalars.dtype)
+        mri_with_boundaries_clipped = mri_with_boundaries.clip_surface(electrode, invert=False, progress_bar=True,
+                                                               crinkle=namespace.crinkle)
+
+        if (mri_with_boundaries.n_cells == mri_with_boundaries_clipped.n_cells):
+            raise ValueError("Radius still too small, nothing was clipped!!!")
+        mri_with_boundaries = mri_with_boundaries_clipped
+
+    else:
+        import IPython
+        IPython.embed()
+        mri_with_boundaries
+        electrode = pyvista.Sphere(radius=np.max(np.abs(mri_with_boundaries.bounds)),
+                                   center=(0, 0 ,0))
+        electrode.cell_data["material"] = np.array([10] * electrode.n_cells,
+                                                   dtype=mri_with_boundaries.cell_data.active_scalars.dtype)
+        mri_with_boundaries_clipped = mri_with_boundaries.clip_surface(electrode, invert=True, progress_bar=True,
+                                                               crinkle=namespace.crinkle)
+
+
+        # mri_with_boundaries = mri_with_boundaries.clean(progress_bar=True)
+    mri_with_boundaries.save(os.path.join(namespace.outdir, os.path.basename(base_outfile) + '_volume.vtu'),
+                             binary=True)
 
 
 if __name__ == '__main__':
